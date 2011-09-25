2bbea0a1517b4fb903fdf947185871b974731aae
Ilia Pavlenkov <dortonway@gmail.com>
2011-09-25 14:19:43 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Смоленская группа пользователей Linux</title>
+<link href="main.css" rel="stylesheet" type="text/css"/>
+<link rel="shortcut icon" href="www.smlug.ru/favicon.ico"/>
+</head>
+<body>
+
+<div align=center>
+<table width=787px cellpadding=0 cellspacing=0 border=0><tr><td colspan='2'>
+
+<a href='http://www.smlug.ru'><img title='Смоленская группа пользователей Linux' class=banner src='logo.jpg'/></a>
+
+</td><tr><td width=50%>&nbsp;</td><td>
+
+<div class=quote>
+<div class=pagequote>When you say "I wrote a program that crashed Windows,"<br/> people just stare at you blankly and say<br/>"Hey, I got those with the system, for free."</div>
+<div class=pagequotesign>Linus Torvalds</div>
+</div>
+
+</td>
+<tr><td colspan='2'>
+
+<p>
+Смоленская группа пользователей Linux (Smolensk Linux Users Group, SmLUG) — неформальное объединение пользователей GNU/Linux и других OpenSource систем на территории Смоленска и Смоленской области.
+</p>
+
+<h3>Что такое Linux?</h3>
+<p>
+Linux - в широком смысле, это свободно распространяемая операционная система с открытым исходным кодом. Говоря более строго, Linux - это название ядра, главной составляющей множества UNIX-совместимых операционных систем, таких как, Debian, Red Hat, Slackware и т.д. и т.п.
+</p>
+<p>
+Linux является одним из главных достижений OpenSource движения. Системы Linux, имея современные технологические характеристики, отличаются от проприетарных операционных систем в первую очредь идеологически. Ядро, разработанное первоначально Линусом Торвальдсом (Linux Torvalds) в Университете Хельсинки (Финляндия), получило признание и поддержку сообществом программистов по всему миру и обросло множеством программ: от утилит командной строки до офисных пакетов и игр. Практически всё программное обеспечение для Linux распространяется бесплатно и в исходных кодах. Например, значительная часть программного обеспечения создаётся в рамках проекта FSF GNU, продукция которого лицензируется, как и само ядро Linux, по лицензии GNU General Public License, предполагающей, кроме прочего, наследование статуса и идеологии "free software" для лицензирумых программ.
+</p>
+
+<!--h3>Группы пользователей Linux (LUGи)</h3>
+
+<p>
+бла-бла-бла
+-->
+
+<h3>Цели LUG</h3>
+<p>
+<ul>
+<li>популяризация Linux;
+<li>обучение работе с UNIX-системами;
+<li>поддержка пользователей;
+<li>обмен мнениями и дистрибутивами.
+</ul>
+</p>
+
+
+<h3>Контакты</h3>
+<ul>
+<li>Почта <a href="mailto:smolensklug@gmail.com">smolensklug@gmail.com</a>
+<li><a href="http://groups.google.com/group/sm-lug">Список рассылки</a> на сервере groups.google.com;</li>
+<li>Jabber-конференция smlug@conference.jabber.ru</li>
+<!--li><a href=''></a></li-->
+</ul>
+
+<!--Вступить в Смоленскую группу пользователй Linux можно... можно :) -->
+
+
+<div class=copyright>&copy; 2010 Смоленская группа пользователей Linux</div>
+
+</td></tr></table>
+</div>
+
+</body>
+</html>
